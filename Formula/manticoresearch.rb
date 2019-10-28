63964228beb918600422a325f69db1455e53b276
Adrian Nuta <adriannuta@gmail.com>
2019-10-28 09:44:20 +0200

    fixes

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -2,7 +2,6 @@ class Manticoresearch < Formula
   desc "Open source text search engine"
   homepage "https://www.manticoresearch.com"
   url "https://github.com/manticoresoftware/manticoresearch/releases/download/3.2.0/manticore-3.2.0-191017-e526a01-release.tar.gz"
-  version "3.2.0"
   sha256 "df6dbcc4df01065fc3cc6328f043b8cef3eb403a28671455cd3c8fc4217e3391"
   head "https://github.com/manticoresoftware/manticoresearch.git"
 
@@ -12,9 +11,8 @@ class Manticoresearch < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "icu4c" => :build
   depends_on "libpq" => :build
-  depends_on "mysql@5.7" => :build
+  depends_on "mysql" => :build
   depends_on "unixodbc" => :build
   depends_on "openssl"
 
@@ -36,12 +34,6 @@ class Manticoresearch < Formula
     (var/"run/manticore").mkpath
     (var/"log/manticore").mkpath
     (var/"data/manticore").mkpath
-  end
-
-  def caveats
-    <<~EOS
-      Config file is located at #{etc}/manticore/sphinx.conf
-    EOS
   end
 
   plist_options :manual => "searchd --config #{HOMEBREW_PREFIX}/etc/manticore/sphinx.conf"
@@ -70,21 +62,20 @@ class Manticoresearch < Formula
     </plist>
   EOS
   end
+
   test do
-    begin
-      (testpath/"sphinx.conf").write <<~EOS
-        searchd {
-          pid_file = searchd.pid
-          binlog_path=#
-        }
-      EOS
-      system bin/"searchd"
-      pid = fork do
-        exec bin/"searchd"
-      end
-    ensure
-      Process.kill(9, pid)
-      Process.wait(pid)
+    (testpath/"sphinx.conf").write <<~EOS
+      searchd {
+        pid_file = searchd.pid
+        binlog_path=#
+      }
+    EOS
+    system bin/"searchd"
+    pid = fork do
+      exec bin/"searchd"
     end
+  ensure
+    Process.kill(9, pid)
+    Process.wait(pid)
   end
 end
